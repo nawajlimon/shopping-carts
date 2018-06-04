7d6291d9dc6c71a1668b89d6666ff2e336fe5665
Jason Vanhalwyn <jvanhalwyn@icloud.com>
2018-06-04 20:50:53 +0200

    add some fields to the Products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801052425) do
+ActiveRecord::Schema.define(version: 2018_06_04_183445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "order_items", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "order_id"
-    t.float    "unit_price"
-    t.integer  "quantity"
-    t.float    "total_price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.float    "subtotal"
-    t.float    "total"
-    t.float    "tax"
-    t.float    "shipping"
+  create_table "order_items", id: :serial, force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "order_id"
+    t.float "unit_price"
+    t.integer "quantity"
+    t.float "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string   "title"
-    t.float    "price"
+  create_table "orders", id: :serial, force: :cascade do |t|
+    t.float "subtotal"
+    t.float "total"
+    t.float "tax"
+    t.float "shipping"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "description"
+    t.string "category"
+    t.string "sizes", default: [], array: true
+    t.string "colors", default: [], array: true
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(title: 'Stringer XY', price: 23.90, description: "Stringer  noir à flocage real gold.\/n \/n
+
+Sa composition 100% coton (combed ring-spun) et sa coupe large vous permettra légèreté et confort lors de vos training ou pour toute autre activité.
+
+Détails :
+
+- Dos nageur, ouvertures XL au niveau des bras.
+
+- Grammage : 160g
+
+Édition limitée à quelques exemplaires.(Signature au dos de l'étiquette extérieure)
+
+LE MANNEQUIN MESURE : 179,5 cm. TAILLE L. (prendre XL si vous souhaitez que le vêtement soit plus long et plus ample).
+
+Si vous ne savez pas quelle taille choisir, veuillez vous référer au 'Guide Des Tailles' disponible sur notre site internet.
+
+
+-> Conseil d'entretien :
+
+Repasser sur envers, séchage en machine à proscrire, lavage 40° maximum.')", colors: ['noir', 'blanc'], sizes: ['S', 'M', 'L', 'XL'], category: 'stringer')
+
